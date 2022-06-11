e343986e350aab61341ccbba55aacd8e8c49a6e0
216229sai <89755692+216229sai@users.noreply.github.com>
2022-06-11 19:54:15 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+
+var first_name = "Sai Swanik";
+var last_name = "Kalachagari"
+
+var a = 10;
+var b = 5;
+
+
+add= a + b;
+console.log(add);
+ var full_name=first_name.concat(last_name);
+console.log(first_name);
+console.log(last_name);
+console.log(full_name)
+console.log(a);
+console.log(b);
+
+
